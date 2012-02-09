2e9d11e0041001f93b8defb2367252b2e9c9769c
ZNickq <ZNickq@ZNickq-PC>
2012-02-09 16:34:55 +0200

    Ah, come on!

--- src/main/java/net/spoutmaterials/spoutmaterials/SmpPackage.java
+++ src/main/java/net/spoutmaterials/spoutmaterials/SmpPackage.java
@@ -65,7 +65,6 @@ public class SmpPackage {
 	private List<Recipe> craftingRecipeList = new ArrayList<Recipe>();
 
 	public SmpPackage(SmpManager smpManager, ZipFile smpFile, String name) {
-		this.name = name;
 		this.smpFile = smpFile;
 		this.smpManager = smpManager;
 		Map<String, YamlConfiguration> materials = new HashMap<String, YamlConfiguration>();
