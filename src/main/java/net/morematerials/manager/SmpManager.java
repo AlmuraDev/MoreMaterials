2ee75ac79f40be550f1152f5d76587e3680243a9
IceReaper <andrem@kulturserver-nrw.de>
2012-07-16 13:03:44 +0200

    - Reimplement crafting recipes.

--- src/main/java/net/morematerials/manager/SmpManager.java
+++ src/main/java/net/morematerials/manager/SmpManager.java
@@ -29,12 +29,22 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.Recipe;
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.inventory.SpoutItemStack;
+import org.getspout.spoutapi.inventory.SpoutShapedRecipe;
+import org.getspout.spoutapi.inventory.SpoutShapelessRecipe;
 import org.getspout.spoutapi.material.Material;
+import org.getspout.spoutapi.material.MaterialData;
+
+import com.github.Zarklord1.FurnaceApi.FurnaceRecipes;
 
 import net.morematerials.MoreMaterials;
 import net.morematerials.materials.CustomShape;
@@ -104,6 +114,14 @@ public class SmpManager {
 		for (Integer i = 0; i < this.blocksList.size(); i++) {
 			this.blocksList.get(i).configureDrops();
 		}
+		
+		// Third loop - Now we can reference all crafting recipes
+		for (String matName : materials.keySet()) {
+			this.registerRecipes(this.plugin.getUtilsManager().getName(smpFile.getName()), matName, materials.get(matName));
+		}
+		
+		// At last load legacyrecipes.yml for compatibility
+		this.loadLegacyRecipes();
 	}
 
 	private void createMaterial(String smpName, String matName, YamlConfiguration yaml, ZipFile smpFile) {
@@ -252,6 +270,91 @@ public class SmpManager {
 			}
 		}
 		return null;
+	}
+
+	private void registerRecipes(String smpName, String matName, YamlConfiguration config) {
+		//TODO Unchecked cast warning remove
+		List<YamlConfiguration> recipes = (List<YamlConfiguration>) config.getList("Recipes");
+		// Make sure we have a valid list.
+		if (recipes == null) {
+			return;
+		}
+		
+		// Get the material object which we want to craft.
+		Material material;
+		if (matName.matches("^[0-9]+$")) {
+			material = this.getMaterial(Integer.parseInt(matName));
+		} else {
+			material = this.getMaterial(smpName, matName).get(0);
+		}
+		
+		for (YamlConfiguration recipe : recipes) {
+			// This is what we want to craft.
+			Integer amount = recipe.getInt("Amount", 1);
+			SpoutItemStack stack = new SpoutItemStack(material, amount);
+			String ingredients = recipe.getString("Ingredients");
+
+			// Building recipe
+			String type = recipe.getString("Type");
+			if (type.equalsIgnoreCase("Furnace")) {
+				// Easy furnace stuff :D
+				Material ingredient;
+				if (ingredients.matches("^[0-9]+$")) {
+					ingredient = MaterialData.getMaterial(Integer.parseInt(ingredients));
+				} else {
+					ingredient = this.getMaterial(smpName, matName).get(0);
+				}
+				//TODO figure out why we need param2 and param3
+				FurnaceRecipes.CustomFurnaceRecipe(new SpoutItemStack(material, amount), ingredient.getRawId(), ingredient.getRawData());
+			} else {
+				Recipe sRecipe = null;
+				// Get recipe type.
+				if (type.equalsIgnoreCase("Shapeless")) {
+					sRecipe = new SpoutShapelessRecipe(stack);
+				} else if (type.equalsIgnoreCase("Shaped")) {
+					sRecipe = new SpoutShapedRecipe(stack).shape("abc","def", "ghi");
+				}
+				
+				// Split ingredients.
+				ingredients = ingredients.replaceAll("\\s{2,}", " ");
+				
+				// Parse all lines
+				Integer currentLine = 0;
+				Integer currentColumn = 0;
+				for (String line : ingredients.split("\\r?\\n")) {
+					for (String ingredientitem : line.split(" ")) {
+						// Skip "air"
+						if (ingredientitem.equals("0")) {
+							currentColumn++;
+							continue;
+						}
+						
+						// Get correct ingredient material
+						Material ingredient;
+						if (ingredients.matches("^[0-9]+$")) {
+							ingredient = MaterialData.getMaterial(Integer.parseInt(ingredients));
+						} else {
+							ingredient = this.getMaterial(smpName, matName).get(0);
+						}
+						
+						// Add the ingredient
+						if (sRecipe instanceof SpoutShapedRecipe) {
+							char a = (char) ('a' + currentColumn + currentLine * 3);
+							((SpoutShapedRecipe) sRecipe).setIngredient(a, ingredient);
+						} else {
+							((SpoutShapelessRecipe) sRecipe).addIngredient(ingredient);
+						}
+					}
+				}
+				// Finaly register recipe.
+				SpoutManager.getMaterialManager().registerSpoutRecipe(sRecipe);
+			}
+		}
+	}
+
+	@Deprecated
+	private void loadLegacyRecipes() {
+		//TODO just load it :D
 	}
 
 }
