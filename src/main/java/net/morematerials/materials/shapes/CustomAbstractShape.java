584e7c1dae1228d556ba24ba20d11cd7bc4f708c
IceReaper <andrem@kulturserver-nrw.de>
2012-09-24 01:14:36 +0200

    - Cleanups.

--- src/main/java/net/morematerials/materials/shapes/CustomAbstractShape.java
+++ src/main/java/net/morematerials/materials/shapes/CustomAbstractShape.java
@@ -5,12 +5,12 @@ import org.getspout.spoutapi.block.design.GenericBlockDesign;
 
 public abstract class CustomAbstractShape extends GenericBlockDesign {
 	
-	protected void calculateLightSource(Integer quad, Float f, Float g, Float h, Float i, Float j, Float k, Float l, Float m, Float n) {
+	protected void calculateLightSource(Integer quad, Float x1, Float y1, Float z1, Float x2, Float y2, Float z2, Float x3, Float y3, Float z3) {
 		Vector normal = new Vector();
 		
-		normal.setX(((g - j) * (k - n)) - ((h - k) * (j - m)));
-		normal.setY(((h - k) * (i - l)) - ((f - i) * (k - n)));
-		normal.setZ(((f - i) * (j - m)) - ((g - j) * (i - l)));
+		normal.setX(((y1 - y2) * (z2 - z3)) - ((z1 - z2) * (y2 - y3)));
+		normal.setY(((z1 - z2) * (x2 - x3)) - ((x1 - x2) * (z2 - z3)));
+		normal.setZ(((x1 - x2) * (y2 - y3)) - ((y1 - y2) * (x2 - x3)));
 		
 		Double length = Math.sqrt((normal.getX() * normal.getX()) + (normal.getY() * normal.getY()) + (normal.getZ() * normal.getZ()));
 		
