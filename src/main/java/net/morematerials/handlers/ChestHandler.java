69c7f6c440fb721c902791e467d8452179d02b2b
Mike Howe <mike@mcsnetworks.com>
2014-03-30 23:08:29 -0500

    Fixed an issue where a exception could be thrown if a chest type custom block is used and residence wasn't being used.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/net/morematerials/handlers/ChestHandler.java
+++ src/main/java/net/morematerials/handlers/ChestHandler.java
@@ -5,19 +5,19 @@ import java.util.Map;
 import net.morematerials.MoreMaterials;
 import net.morematerials.handlers.GenericHandler;
 
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
-import org.getspout.spout.block.SpoutCraftBlock;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.protection.ClaimedResidence;
 
 public class ChestHandler extends GenericHandler {
-	
+
 	public void init(MoreMaterials plugin) {}
 
 	public void shutdown() {}
@@ -33,28 +33,33 @@ public class ChestHandler extends GenericHandler {
 		// Check Player Permissions
 		if (!sPlayer.hasPermission("morematerials.handlers.chest")) {
 			return;
-		}        		
+		}
 
 		Block block = playerEvent.getClickedBlock();
-		ClaimedResidence res = Residence.getResidenceManager().getByLoc(block.getLocation());
-		if (res != null) {
-			if (res.getPermissions().playerHas(sPlayer.getName(),"container", true)) {				
-				if (block != null) {		
-					try {
-						// Its possible for a class cast exception to be thrown if part of the custom block data is missing, fixed this by ignoring the exception
-						Chest chest = (Chest)block.getState();
-						if (chest != null) {
-							Inventory inv = chest.getBlockInventory();
-							if (inv != null) {
-								sPlayer.openInventory(inv);
-							}
-						}	
-					} catch (Exception exception) {
-						// Catch Class Cast Exception and ignore it
-						return;
-					} 
 
+		// Residence Flag Checker
+		if (!Bukkit.getPluginManager().isPluginEnabled("Residence")) {
+			ClaimedResidence res = Residence.getResidenceManager().getByLoc(block.getLocation());
+			if (res != null) {
+				if (!res.getPermissions().playerHas(sPlayer.getName(),"container", true)) {
+					return;
 				}
+			}
+		}
+		
+		if (block != null) {
+			try {
+				// Its possible for a class cast exception to be thrown if part of the custom block data is missing, fixed this by ignoring the exception
+				Chest chest = (Chest)block.getState();
+				if (chest != null) {
+					Inventory inv = chest.getBlockInventory();
+					if (inv != null) {
+						sPlayer.openInventory(inv);
+					}
+				}	
+			} catch (Exception exception) {
+				// Catch Class Cast Exception and ignore it
+				return;
 			}
 		}
 	}
