230c665edf496c88496920a2a148ddf32ad67057
Steven Downer <grinch@outlook.com>
2014-04-23 23:38:46 -0500

    Update POM.xml
    - Apply license headers to all files
    - Add LICENSE.txt
    - Add HEADER.txt
    
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/DataDebugHandler.java
+++ src/main/java/DataDebugHandler.java
@@ -1,20 +1,18 @@
 /*
- * This file is part of MoreMaterials.
- * 
- * Copyright (c) 2012 Andre Mohren (IceReaper)
- * 
- * The MIT License
- * 
+ * This file is part of MoreMaterials, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2013 AlmuraDev <http://www.almuradev.com/>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 import java.util.Map;
 
 import net.morematerials.MoreMaterials;
